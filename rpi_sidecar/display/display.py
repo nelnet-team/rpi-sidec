f35ce0d1f9ea7b6ab7b2d23accd5df954fa7ca91
Lee Nelson <lnelson@nelnet.org>
2020-03-11 16:57:40 -0700

    Switch screens with gpio

--- rpi_sidecar/display/display.py
+++ rpi_sidecar/display/display.py
@@ -5,6 +5,7 @@ import time
 import asyncio
 from wifi import wifi_status
 from ip_interface import interface_info
+from system import sys_info
 
 
 class DisplayInfo:
@@ -18,6 +19,11 @@ class DisplayInfo:
         self.ip_mask = ""
         self.if_stats = ""
         self.interface = "wlan0"
+        self.cpu_usage = ""
+        self.mem_usage = ""
+        self.disk_usage = ""
+        self.uptime = ""
+        self.sys_info = sys_info.SysInfo()
         self.interface_info = interface_info.InterfaceInfo(self.interface)
         self.wifi_info = wifi_status.WifiStatus(self.interface)
         self.redraw = True
@@ -25,7 +31,7 @@ class DisplayInfo:
     def terminate(self):
         self._running = False
 
-    def draw_info(self):
+    def draw_interface(self):
         top = "{} {}".format(self.interface, self.wifi_ssid)
         status = "State: {}".format(self.wifi_state)
         ipaddr = "{}/{}".format(self.ip_addr, self.ip_mask)
@@ -36,9 +42,16 @@ class DisplayInfo:
             draw.text((0, 24), ipaddr, fill="white")
             draw.text((0, 32), self.if_stats, fill="white")
 
+    def draw_sysinfo(self):
+        with canvas(self.device) as draw:
+            draw.text((0, 0), self.cpu_usage, fill="white")
+            draw.text((0, 8), self.uptime, fill="white")
+            draw.text((0, 16), self.mem_usage, fill="white")
+            draw.text((0, 24), self.disk_usage, fill="white")
+
     async def display_interface(self):
-        try:
-            while True:
+        while True:
+            try:
                 ip_addr = self.interface_info.get_ip()
                 if ip_addr != self.ip_addr:
                     self.redraw = True
@@ -65,10 +78,26 @@ class DisplayInfo:
                     self.wifi_ssid = wifi_ssid
 
                 if self.redraw:
-                    self.draw_info()
+                    print("Testing if redraw needed")
+                    self.draw_interface()
                     self.redraw = False
 
                 await asyncio.sleep(self.interval)
 
-        except asyncio.CancelledError:
-            return ()
+            except asyncio.CancelledError:
+                self.redraw = True
+                return ()
+
+    async def display_sys_info(self):
+        while True:
+            try:
+                self.cpu_usage = self.sys_info.get_cpu_usage()
+                self.uptime = self.sys_info.get_uptime()
+                self.mem_usage = self.sys_info.get_mem_usage()
+                self.disk_usage = self.sys_info.get_disk_usage('/')
+                self.draw_sysinfo()
+                print("Finished drawing sys_info")
+                await asyncio.sleep(self.interval)
+
+            except asyncio.CancelledError:
+                return ()
--- rpi_sidecar/rpi_sidecar.py
+++ rpi_sidecar/rpi_sidecar.py
@@ -1,14 +1,63 @@
 """Main module."""
 import asyncio
 from display import display
+import time
+import RPi.GPIO as GPIO
+import functools
 
 
-async def main(obj):
-    await obj.display_interface()
+def gpio_interrupt():
+    print("Entering gpio_interrupt")
+    time.sleep(0.2)
+    pin = 17
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.wait_for_edge(pin, GPIO.FALLING)
+    return (True)
+
+
+def run_in_executor(f):
+    # Decorator to run a function in an executor
+    @functools.wraps(f)
+    def inner(*args, **kwargs):
+        loop = asyncio.get_running_loop()
+        return loop.run_in_executor(None, lambda: f(*args, **kwargs))
+    return inner
+
+# Create an executor to make the interrupt awaitable
+@run_in_executor
+def wait_for_interrupt():
+    print("Entering wait_for_interrupt")
+    resp = gpio_interrupt()
+    return (resp)
+
+
+async def process_interrupt(obj):
+    print("Entering process_interrupt")
+    res = await wait_for_interrupt()
+    print("Interupt triggered")
+
+
+async def display_info(obj):
+    while True:
+        try:
+            task = asyncio.create_task(obj.display_sys_info())
+            await process_interrupt(obj)
+            task.cancel()
+            task = asyncio.create_task(obj.display_interface())
+            await process_interrupt(obj)
+            task.cancel()
+        except asyncio.CancelledError:
+            return ()
+
+
+async def main():
+    obj = display.DisplayInfo()
+    await asyncio.create_task(display_info(obj))
 
 if __name__ == '__main__':
-    displayinfo = display.DisplayInfo()
     try:
-        asyncio.run(main(displayinfo))
+        asyncio.run(main())
     except KeyboardInterrupt:
+        # loop.stop
         print('Terminated\n')
